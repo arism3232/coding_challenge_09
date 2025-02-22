617f7d841cd9c295326605b01691fb531aa71664
Aritree Hoque <aritree.hoque@gmail.com>
2025-02-22 02:05:35 -0500

    Task 2 - Created Manager Class with Inheritance

--- cc_9.js
+++ cc_9.js
@@ -20,3 +20,27 @@ console.log(emp1.getDetails());
 
 console.log(emp1.calculateAnnualSalary()); 
 // Expected output: 60000
+
+// Task2- Creating a Manager Class (Inheriting & Mehtod Overriding)
+class Manager extends Employee {
+    constructor(name, id, department, salary, teamSize) {
+        super(name, id, department, salary);
+        this.teamSize = teamSize;
+    }
+    getDetails() {
+        return `Manager: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: ${this.salary}, Team Size: ${this.teamSize}`
+    }
+    calculateBonus() {
+        return this.salary * 0.10 * 12
+    }
+    calculateAnnualSalary() {
+        return this.salary * 12 + this.calculateBonus();
+    }
+};
+// Test Cases
+const mgr1 = new Manager("John Smith", 201, "IT", 8000, 5);
+console.log(mgr1.getDetails());
+// Expected output: "Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5"
+
+console.log(mgr1.calculateBonus()); 
+// Expected output: 9600
