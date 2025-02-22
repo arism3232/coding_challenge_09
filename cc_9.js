8a46116e096283a63f12be6deef0ac2ad688a3bc
Aritree Hoque <aritree.hoque@gmail.com>
2025-02-22 01:57:44 -0500

    Task 1 - Created Employee Class

---
+++ cc_9.js
@@ -0,0 +1,22 @@
+// Task1- Creating an Employee CLass
+class Employee {
+    constructor(name, id, department, salary) {
+        this.name = name;
+        this.id = id;
+        this.department = department;
+        this.salary = salary;
+    };
+    getDetails() {
+        return `Employee: ${this.name}, ID: ${this.id}, Department: ${this.department}, Salary: $${this.salary}`;
+        }
+    calculateAnnualSalary() {
+        return this.salary * 12
+    };
+};
+// Test Cases
+const emp1 = new Employee("Alice Johnson", 101, "Sales", 5000);
+console.log(emp1.getDetails()); 
+// Expected output: "Employee: Alice Johnson, ID: 101, Department: Sales, Salary: $5000"
+
+console.log(emp1.calculateAnnualSalary()); 
+// Expected output: 60000
